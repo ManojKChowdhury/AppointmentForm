3cc0d63ba8f40d48213f52bd4c41385c0f6a3538
Manoj Chowdhury <manojc009@gmail.com>
2023-11-04 21:23:30 +0530

    updates sing up form

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,148 +1,147 @@
-
 <template>
-    <div class="card">
-        <DataTable v-model:expandedRows="expandedRows" :value="products" dataKey="id"
-                @rowExpand="onRowExpand" @rowCollapse="onRowCollapse" tableStyle="min-width: 60rem">
-            <!-- <template #header>
-                <div class="flex flex-wrap justify-content-end gap-2">
-                    <Button text icon="pi pi-plus" label="Expand All" @click="expandAll" />
-                    <Button text icon="pi pi-minus" label="Collapse All" @click="collapseAll" />
-                </div>
-            </template> -->
-            <Column expander style="width: 5rem" />
-            <Column field="description" header="Job Description"></Column>
-            <Column field="clientName" header="Client Name"></Column>
-            <Column field="clientAddress" header="Address">
-                <template #body="slotProps">
-                    {{ formatAddress(slotProps.data.clientAddress) }}
-                </template>
+  <div class="card">
+    <DataTable v-model:expandedRows="expandedRows" :value="products" dataKey="id"
+               @rowExpand="onRowExpand" @rowCollapse="onRowCollapse" tableStyle="min-width: 60rem">
+      <!-- <template #header>
+          <div class="flex flex-wrap justify-content-end gap-2">
+              <Button text icon="pi pi-plus" label="Expand All" @click="expandAll" />
+              <Button text icon="pi pi-minus" label="Collapse All" @click="collapseAll" />
+          </div>
+      </template> -->
+      <Column expander style="width: 5rem"/>
+      <Column field="description" header="Job Description"></Column>
+      <Column field="clientName" header="Client Name"></Column>
+      <Column field="clientAddress" header="Address">
+        <template #body="slotProps">
+          {{ formatAddress(slotProps.data.clientAddress) }}
+        </template>
+      </Column>
+      <Column field="timeRequired" header="Time"></Column>
+      <Column field="jobDates" header="Date"></Column>
+      <Column header="Status">
+        <template #body="slotProps">
+          <Tag :value="slotProps.data.jobStatus" :severity="getSeverity(slotProps.data)"/>
+        </template>
+      </Column>
+      <template #expansion="slotProps">
+        <div class="p-3">
+          <h5>Nurse Appointments for {{ slotProps.data.clientName }} requirement</h5>
+          <DataTable :value="slotProps.data.users">
+            <Column field="userId" header="Id" sortable></Column>
+            <Column field="basicDetails" header="Name" sortable>
+              <template #body="slotProps">
+                {{ formatName(slotProps.data.basicDetails) }}
+              </template>
             </Column>
-            <Column field="timeRequired" header="Time"></Column>
-            <Column field="jobDates" header="Date"></Column>
-            <Column header="Status">
-                <template #body="slotProps">
-                    <Tag :value="slotProps.data.jobStatus" :severity="getSeverity(slotProps.data)" />
-                </template>
+            <Column field="basicDetails.gender" header="Gender"></Column>
+            <Column field="pincode" header="Pincode" sortable>
+              <template #body="slotProps">
+                {{ formatPincode(slotProps.data.userAddress) }}
+              </template>
             </Column>
-            <template #expansion="slotProps">
-                <div class="p-3">
-                    <h5>Nurse Appointments for {{ slotProps.data.clientName }} requirement</h5>
-                    <DataTable :value="slotProps.data.users" >
-                        <Column field="userId" header="Id" sortable></Column>
-                        <Column field="basicDetails" header="Name" sortable>
-                            <template #body="slotProps">
-                                {{ formatName(slotProps.data.basicDetails) }}
-                            </template>
-                        </Column>
-                        <Column field="basicDetails.gender" header="Gender"></Column>
-                        <Column field="pincode" header="Pincode" sortable>
-                            <template #body="slotProps">
-                                {{ formatPincode(slotProps.data.userAddress) }}
-                            </template>
-                        </Column>
-                        <Column field="educationHistory" header="Education" sortable>
-                            <template #body="slotProps">
-                              <Tag v-for="edu in slotProps.data.educationHistory" :value="formatEducation(edu)" />
-                            </template>
-                        </Column>
-                        <Column field="workExperience" header="Experience" sortable>
-                            <template #body="slotProps">
-                                <Tag v-for="exp in slotProps.data.workExperience" :value="formatWork(exp)" />
-                            </template>
-                        </Column>
-                        <Column field="userId" header="Approval" headerStyle="width:4rem">
-                            <template #body="slotProps">
-                                <Button @click="onApprove(slotProps.data.userId)">Approve</Button>
-                            </template>
-                        </Column>
-                    </DataTable>
-                </div>
-            </template>
-        </DataTable>
-        <Toast />
-    </div>
+            <Column field="educationHistory" header="Education" sortable>
+              <template #body="slotProps">
+                <Tag v-for="edu in slotProps.data.educationHistory" :value="formatEducation(edu)"/>
+              </template>
+            </Column>
+            <Column field="workExperience" header="Experience" sortable>
+              <template #body="slotProps">
+                <Tag v-for="exp in slotProps.data.workExperience" :value="formatWork(exp)"/>
+              </template>
+            </Column>
+            <Column field="userId" header="Approval" headerStyle="width:4rem">
+              <template #body="slotProps">
+                <Button @click="onApprove(slotProps.data.userId)">Approve</Button>
+              </template>
+            </Column>
+          </DataTable>
+        </div>
+      </template>
+    </DataTable>
+    <Toast/>
+  </div>
 </template>
 
 <script>
-import { ProductService } from '~/api/ProductService';
+import {ProductService} from '~/api/ProductService';
 
 export default {
-    data() {
-        return {
-            products: null,
-            expandedRows: []
-        };
+  data() {
+    return {
+      products: null,
+      expandedRows: []
+    };
+  },
+  mounted() {
+    ProductService.getProductsWithOrdersSmall().then((data) => (this.products = data));
+  },
+  methods: {
+    onApprove(userId) {
+      console.log("Found User Id", userId)
     },
-    mounted() {
-        ProductService.getProductsWithOrdersSmall().then((data) => (this.products = data));
+    onRowExpand(event) {
+      // this.$toast.add({ severity: 'info', summary: 'Product Expanded', detail: event.data.name, life: 3000 });
     },
-    methods: {
-        onApprove(userId){
-            console.log("Found User Id", userId)
-        },
-        onRowExpand(event) {
-            // this.$toast.add({ severity: 'info', summary: 'Product Expanded', detail: event.data.name, life: 3000 });
-        },
-        onRowCollapse(event) {
-            // this.$toast.add({ severity: 'success', summary: 'Product Collapsed', detail: event.data.name, life: 3000 });
-        },
-        expandAll() {
-            this.expandedRows = this.products.filter((p) => p.id);
-        },
-        collapseAll() {
-            this.expandedRows = null;
-        },
-        formatName(details) {
-            return `${details.firstName} ${details.lastName} `
-        },
-        formatAddress(userAddress) {
-            return `${userAddress.addressLine}, ${userAddress.city}, ${userAddress.pincode}`;
-        },
-        formatPincode(userAddress){
-           return userAddress?.postalCode; 
-        },
-        formatCurrency(value) {
-            return value.toLocaleString('en-US', { style: 'currency', currency: 'INR' });
-        },
-        formatEducation({ degree, college }) {
-            return `${degree}, ${college}`;
-        },
-        formatWork({ jobTitle,  institute}){
-           return `${jobTitle}, ${institute}`;
-        },
-        getSeverity(product) {
-            switch (product.jobStatus) {
-                case 'Submitted':
-                    return 'success';
+    onRowCollapse(event) {
+      // this.$toast.add({ severity: 'success', summary: 'Product Collapsed', detail: event.data.name, life: 3000 });
+    },
+    expandAll() {
+      this.expandedRows = this.products.filter((p) => p.id);
+    },
+    collapseAll() {
+      this.expandedRows = null;
+    },
+    formatName(details) {
+      return `${details.firstName} ${details.lastName} `
+    },
+    formatAddress(userAddress) {
+      return `${userAddress.addressLine}, ${userAddress.city}, ${userAddress.pincode}`;
+    },
+    formatPincode(userAddress) {
+      return userAddress?.postalCode;
+    },
+    formatCurrency(value) {
+      return value.toLocaleString('en-US', {style: 'currency', currency: 'INR'});
+    },
+    formatEducation({degree, college}) {
+      return `${degree}, ${college}`;
+    },
+    formatWork({jobTitle, institute}) {
+      return `${jobTitle}, ${institute}`;
+    },
+    getSeverity(product) {
+      switch (product.jobStatus) {
+        case 'Submitted':
+          return 'success';
 
-                case 'Posted':
-                    return 'warning';
+        case 'Posted':
+          return 'warning';
 
-                case 'OUTOFSTOCK':
-                    return 'danger';
+        case 'OUTOFSTOCK':
+          return 'danger';
 
-                default:
-                    return null;
-            }
-        },
-        getOrderSeverity(order) {
-            switch (order.status) {
-                case 'DELIVERED':
-                    return 'success';
+        default:
+          return null;
+      }
+    },
+    getOrderSeverity(order) {
+      switch (order.status) {
+        case 'DELIVERED':
+          return 'success';
 
-                case 'CANCELLED':
-                    return 'danger';
+        case 'CANCELLED':
+          return 'danger';
 
-                case 'PENDING':
-                    return 'warning';
+        case 'PENDING':
+          return 'warning';
 
-                case 'RETURNED':
-                    return 'info';
+        case 'RETURNED':
+          return 'info';
 
-                default:
-                    return null;
-            }
-        }
+        default:
+          return null;
+      }
     }
+  }
 };
 </script>
--- pages/signup.vue
+++ pages/signup.vue
@@ -2,17 +2,17 @@
   <Card role="form" class="w-3 m-auto">
     <template #header>
       <!--  logo goes here
-      <img alt="user header" src=""/>
+      <img alt="logo" src=""/>
       -->
     </template>
     <template #title>Create New Account</template>
     <template #subtitle>Marketplace For Home Healthcare</template>
     <template #content>
-      <form @submit.prevent="submitForm">
+      <form @submit="submitForm">
         <div class="flex flex-column gap-2">
           <div class="flex flex-column gap-2">
             <label for="name">Full name*</label>
-            <InputText id="name" type="text" v-model="name" placeholder="Your Full name"/>
+            <InputText id="name" type="text" v-model="name" placeholder="Your Full name" required/>
           </div>
           <div class="flex flex-column gap-2">
             <label for="email">Email Address*</label>
@@ -20,18 +20,19 @@
           </div>
           <div class="flex flex-column gap-2">
             <label for="phone">Phone Number*</label>
-            <InputText id="phone" type="text" v-model="phone" placeholder="+91 Registered Number"/>
+            <InputNumber id="phone" type="text" v-model="phone" placeholder="+91 Registered Number"
+                         :useGrouping="false" prefix="+91 "/>
           </div>
           <div class="flex flex-column gap-2 full-width">
-            <label for="password">Password</label>
+            <label for="password">Password*</label>
             <Password id="password" v-model="password" placeholder="unique password"
                       :class="{ 'p-invalid': passwordErrorMessage }" @change="passwordChangeHandler" toggle-mask/>
-            <small class="p-error" id="text-error" v-if="passwordErrorMessage !== ''">>{{
+            <small class="p-error" id="text-error" v-if="passwordErrorMessage !== ''">{{
                 passwordErrorMessage
               }}</small>
           </div>
           <div class="flex flex-column gap-2 full-width">
-            <label for="confirmPassword">Re-enter Password</label>
+            <label for="confirmPassword">Re-enter Password*</label>
             <Password id="confirmPassword" v-model="confirmPassword" placeholder="Re-enter unique password"
                       :class="{ 'p-invalid': passwordErrorMessage }" @change="confirmPasswordChangeHandler"
                       toggle-mask/>
@@ -41,8 +42,10 @@
       </form>
     </template>
     <template #footer>
-      <Button icon="pi pi-check" type="submit" label="Sign Up"/>
-      <Button icon="pi pi-times" label="Cancel" severity="secondary" style="margin-left: 0.5em"/>
+      <div class="flex justify-content-between flex-wrap">
+        <Button icon="pi pi-check" type="submit" label="Sign Up" :loading="loading" @click="submitForm"/>
+        <Button icon="pi pi-times" label="Cancel" severity="secondary" @click="cancel"/>
+      </div>
     </template>
   </Card>
 </template>
@@ -53,15 +56,22 @@ export default {
     return {
       name: '',
       email: '',
-      phone: '',
+      phone: null,
       password: '',
       confirmPassword: '',
-      passwordErrorMessage: ''
+      passwordErrorMessage: '',
+      loading: false
     }
   },
   methods: {
     submitForm() {
-      //
+      if (this.isFormValid()) {
+        this.loading = true;
+        // save data
+        setTimeout(() => {
+          this.loading = false;
+        }, 1000);
+      }
     },
     confirmPasswordChangeHandler() {
       this.matchPasswords();
@@ -71,6 +81,21 @@ export default {
     },
     matchPasswords() {
       this.passwordErrorMessage = this.password === this.confirmPassword ? '' : 'Passwords do not match';
+    },
+    cancel() {
+      this.$data.name = '';
+      this.$data.email = '';
+      this.$data.phone = null;
+      this.$data.password = '';
+      this.$data.confirmPassword = '';
+    },
+    isFormValid() {
+      if (this.name.trim() === '') return false;
+      if (this.email.trim() === '') return false;
+      if (this.phone === null) return false;
+      if (this.password.trim() === '') return false;
+      if (this.confirmPassword.trim() === '') return false;
+      return true;
     }
   }
 }
